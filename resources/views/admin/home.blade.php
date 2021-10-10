bc600132fd794a84d655cf2cf6b270fed2352601
leofraijo <leofraijo14@gmail.com>
2021-10-09 19:06:48 -0700

    adding content buttons and positioning the sidebar and header

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -3,5 +3,35 @@
 @section('title', 'Inicio')
 
 @section('content')
+
     @include('layouts.adminLayout.header')
+    <div class="content">
+        @include('layouts.adminLayout.sidebar')        
+
+        <main class="home-content">
+            <div class="buttons-container">
+                
+                <button class="home-button">             
+                    <i class="fas fa-book size-icon"></i>
+                    <h4>Libros</h4>                                    
+                </button>             
+            
+                <button class="home-button">  
+                    
+                    <i class="fas fa-book size-icon"></i>
+                    <h4>Prestamos</h4>                   
+                    
+                </button>                
+                
+                <button class="home-button">                   
+                    <i class="fas fa-book size-icon"></i>
+                    <h4>Devoluciones</h4>
+                </button> 
+                
+            </div>
+            
+        </main>
+    </div>
+
+    
 @endsection
---
+++ resources/views/layouts/adminLayout/sidebar.blade.php
@@ -0,0 +1,12 @@
+<nav class="side_bar">
+
+    <img class="profile_img "src="img/imagenPerfil.png" alt="Imagen">
+
+    <ul class="ul_content">
+        <li><a href="#">Inicio</a></li>
+        <li><a href="#">libros</a></li>
+        <li><a href="#">Devoluciones</a></li>
+        <li><a href="#">Prestamo</a></li>
+    </ul>
+
+</nav>
