40e57293b5b0fbd650133683c054d195b9826bfb
leofraijo <leofraijo14@gmail.com>
2021-10-26 13:20:55 -0700

    first create-book view

--- resources/views/layouts/adminLayout/layout.blade.php
+++ resources/views/layouts/adminLayout/layout.blade.php
@@ -8,6 +8,7 @@
     <link rel="stylesheet" href="{{asset('css/admin/home.css')}}">
     <link rel="stylesheet" href="{{asset('css/admin/edit.css')}}">
     <link rel="stylesheet" href="{{asset('css/admin/show.css')}}">
+    <link rel="stylesheet" href="{{asset('css/admin/create.css')}}">
     <link rel="stylesheet" href="
     https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css">
     <title>@yield('title')</title>
