2f6d914510d6fa93c44628ac50fddaae5630256c
leofraijo <leofraijo14@gmail.com>
2021-11-25 13:29:04 -0700

    fixed copy_statuses seeding error

--- database/seeders/CopySeeder.php
+++ database/seeders/CopySeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Copy;
+use App\Models\CopyStatus;
 use Illuminate\Database\Seeder;
 
 class CopySeeder extends Seeder
@@ -14,6 +15,18 @@ class CopySeeder extends Seeder
      */
     public function run()
     {
+        $copyStatus = new CopyStatus();
+        $copyStatus->name = "disponible";
+        $copyStatus->save();
+
+        $copyStatus2 = new CopyStatus();
+        $copyStatus2->name = "prestado";
+        $copyStatus2->save();
+
+        $copyStatus3 = new CopyStatus();
+        $copyStatus3->name = "extraviado";
+        $copyStatus3->save();
+
         Copy::factory()->count(100)->create();
     }
 }
